004fba96b080a64550e25427135ec4a824b0e982
Kimmo Lepola <kimmo.lepola@gmail.com>
2021-03-14 18:11:25 +0200

    books section ok

--- src/components/Recommended-old.js
+++ src/components/Recommended-old.js
@@ -66,7 +66,32 @@ const Recommended = ({ show, user, ALL_BOOKS }) => {
         <Typography style={{ marginTop: 5 }} variant="subtitle2" id="subtitle" component="div">
         Books in your favorite genre {genre}
         </Typography>
-        <h2> </h2>
+        <TableContainer component={Paper}>
+          <Table className={classes.table} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Dessert (100g serving)</TableCell>
+                <TableCell align="right">Calories</TableCell>
+                <TableCell align="right">Fat&nbsp;(g)</TableCell>
+                <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+                <TableCell align="right">Protein&nbsp;(g)</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow key={row.name}>
+                  <TableCell component="th" scope="row">
+                    {row.name}
+                  </TableCell>
+                  <TableCell align="right">{row.calories}</TableCell>
+                  <TableCell align="right">{row.fat}</TableCell>
+                  <TableCell align="right">{row.carbs}</TableCell>
+                  <TableCell align="right">{row.protein}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
         <table>
           <tbody>
             <tr>
